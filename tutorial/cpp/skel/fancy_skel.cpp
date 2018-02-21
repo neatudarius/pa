193797dc75f600d85630bcdffe1f8b884c8c02d4
Darius-Florentin NEATU <neatudarius@gmail.com>
2018-02-21 15:48:00 +0200

    add makefile & skel

---
+++ tutorial/cpp/skel/fancy_skel.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	freopen("gigel.in",  "r", stdin);   // daca las linia asta citesc din fisier
+    freopen("gigel.out", "w", stdout);  // daca las linia asta afisez in fisier
+    
+    // acum pot folosi cin si cout sa citesc/afisez din/in fisier daca am lasat liniile
+    // de mai sus decomentate
+    
+	cout << "pwp de la Gigel\n"; // aici am scris un mesaj in gigel.out
+
+	return 0;
+}
---
+++ tutorial/cpp/skel/skel.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <fstream>
+#include <map>
+#include <queue>
+#include <stack>
+#include <set>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	freopen("gigel.in",  "r", stdin);   // daca las linia asta citesc din fisier
+    freopen("gigel.out", "w", stdout);  // daca las linia asta afisez in fisier
+    
+    // acum pot folosi cin si cout sa citesc/afisez din/in fisier daca am lasat liniile
+    // de mai sus decomentate
+    
+	cout << "pwp de la Gigel\n"; // aici am scris un mesaj in gigel.out
+
+	return 0;
+}
