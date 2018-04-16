259037b1a1ff18f3bf0b5bcf86f69eaff565526d
Alexandru-Gabriel POPESCU (87516) <alexandru.popescu97@stud.acs.upb.ro>
2018-04-16 08:24:34 -0400

    fix comments for bcc_tarjan

--- lab/skel_graph/traversal/_examples/bcc/tarjan/main.cpp
+++ lab/skel_graph/traversal/_examples/bcc/tarjan/main.cpp
@@ -34,10 +34,13 @@ private:
   // stiva folosita pentru a reconstrui componentele biconexe
   stack<edge> sc;
 
-  // nivelul / ordinea de vizitare
-  vector<int> idx;
+  // ordinea de vizitare
+  // found[node] = timpul de start a lui node in parcurgerea DFS
+  // in laborator found se numeste idx
+  vector<int> found;
 
-  // min { idx[u] | u este accesibil din v }
+  // low_link[node] = min { found[x] | x este accesibil din node }
+  // adica timpul minim al unui nou
   vector<int> low_link;
 
   // vector in care retin punctele de articulatie
@@ -51,12 +54,13 @@ private:
 
   void read_input() {
     cin >> n >> m;
-    idx = vector<int>(n + 1, -1);
+    found = vector<int>(n + 1, -1);
     low_link = vector<int>(n + 1, 0);
     parent = vector<int>(n + 1, 0);
-    int x, y;
     for (int i = 1; i <= m; ++i) {
+      int x, y;
       cin >> x >> y;
+
       adj[x].push_back(y);
       adj[y].push_back(x);
     }
@@ -65,14 +69,93 @@ private:
   void get_result() { tarjan(); }
 
   void tarjan() {
-    int index = 0;
+    // momentul curent de start
+    // pe masura ce vizita nodurile el va creste (++)
+    int current_start = 0;
+
     for (int i = 1; i <= n; ++i) {
-      if (idx[i] == -1) {
+      if (found[i] ==
+          -1) { // acest nod nu a fost descoperit, deci il putem folosi
         // marcam nodul i ca fiind radacina
         parent[i] = 0;
+
         // pornim o noua cautare din nodul respectiv
-        dfs(i, index);
+        dfs(i, current_start);
       }
+    }
+  }
+
+  void dfs(int node, int &current_start) {
+    // incep un nou nod, deci un nod timp de start
+    ++current_start;
+
+    // atat found, cat si low_link vor primi valoarea lui current_start
+    found[node] = current_start;
+    low_link[node] = current_start;
+
+    // initializez numarul de copii al nodului curent cu 0
+    int children = 0;
+
+    for (auto &vecin : adj[node]) {
+      if (found[vecin] == -1) { // deci il pot vizita
+        // parintele nodului in care ma duc este chiar nodul curent
+        parent[vecin] = node;
+
+        // cresc numarul de copii
+        ++children;
+
+        // retin muchiile de avansare
+        sc.push(edge(node, vecin));
+
+        // pornesc un nou dfs
+        dfs(vecin, current_start);
+
+        // updatez low_link:
+        // - low_link[node]  = timpul de start cel mai mic pe care NODE  il
+        // cunoaste
+        // - low_link[vecin] = timpul de start cel mai mic pe care VECIN il
+        // cunoaste
+        // Tot ce acceseaza vecin (Ex. vecin - ... - x) poate accesa si node
+        // (ex. node - vecin - ... - x)
+        low_link[node] = std::min(low_link[node], low_link[vecin]);
+
+        // CUT VERTEX - caz 2:
+        // daca nu au existat muchii de intoarcere spre un STRAMOS al lui node
+        // inseamna ca nodul curent este punct de articulatie
+        // adica VECIN nu a putut sa sara inapoi in arbore PESTE node
+        if (low_link[vecin] >= found[node]) {
+          // radacina este tratata separat (adica conditia de mai sus nu este
+          // pentru radacina)
+          if (parent[node] != 0) {
+            cut_vertex.push_back(node);
+          }
+
+          // daca am gasit un punct de articulatie inseamna ca am descoperit
+          // o noua componenta biconexa
+          get_bcc(edge(node, vecin));
+        }
+
+        // daca nu au existat muchii de intoarcere spre node sau spre un parinte
+        // al lui node
+        // inseamna ca muchia (node, vecin) este muchie critica
+        if (low_link[vecin] > found[node]) {
+          critical_edges.push_back(edge(node, vecin));
+        }
+      } else {
+        // !!!graful fiind neorientat as updata fiecare copil in functie de
+        // parintele sau
+        // trebuie sa am grija sa nu fac asta
+        if (vecin != parent[node]) {
+          // am gasit o muchie de intoarcere
+          low_link[node] = std::min(low_link[node], found[vecin]);
+        }
+      }
+    }
+
+    // CUT VERTEX - caz 1:
+    // daca nodul curent este radacina si are mai mult de 2 copii
+    if (parent[node] == 0 && children >= 2) {
+      cut_vertex.push_back((node));
     }
   }
 
@@ -87,6 +170,8 @@ private:
     while (current_edge != target_edge) {
       current_edge = sc.top();
       sc.pop();
+
+      // adaug capetele muchiei in bcc
       current_bcc.push_back(current_edge.first);
       current_bcc.push_back(current_edge.second);
     }
@@ -97,72 +182,9 @@ private:
     sort(current_bcc.begin(), current_bcc.end());
     auto it = unique(current_bcc.begin(), current_bcc.end());
     current_bcc.erase(it, current_bcc.end());
+
+    // current_bcc este o BCC valida
     bcc.push_back(current_bcc);
-  }
-
-  void dfs(int node, int &index) {
-    // atat idx, cat si low_link vor primi valoarea lui index
-    // index = nivelul la care am ajuns cu parcurgerea
-    idx[node] = index;
-    low_link[node] = index;
-
-    // cresc nivelul, deoarece vecinii lui node vor fi pe nivelul index + 1
-    ++index;
-
-    // initializez numarul de copii al nodului curent cu 0
-    int children = 0;
-
-    for (auto &vecin : adj[node]) {
-      if (idx[vecin] == -1) {
-        // parintele nodului in care ma duc este chiar nodul curent
-        parent[vecin] = node;
-
-        // cresc numarul de copii
-        ++children;
-
-        // retin muchiile de avansare
-        sc.push(edge(node, vecin));
-
-        // pornesc un nou dfs
-        dfs(vecin, index);
-
-        // updatez low_link dupa modelul componente tare conexe
-        low_link[node] = std::min(low_link[node], low_link[vecin]);
-
-        // daca nu au existat muchii de intoarcere spre un parinte al lui node
-        // inseamna ca nodul curent este punct de articulatie
-        if (low_link[vecin] >= idx[node]) {
-          // radacina este tratata separat
-          if (parent[node] != 0) {
-            cut_vertex.push_back(node);
-          }
-
-          // daca am gasit un punct de articulatie inseamna ca am descoperit
-          // o noua componenta biconexa
-          get_bcc(edge(node, vecin));
-        }
-
-        // daca nu au existat muchii de intoarcere spre node sau spre un parinte
-        // al lui node
-        // inseamna ca muchia (node, vecin) este muchie critica
-        if (low_link[vecin] > idx[node]) {
-          critical_edges.push_back(edge(node, vecin));
-        }
-      } else {
-        // !!!graful fiind neorientat as updata fiecare copil in functie de
-        // parintele sau
-        // trebuie sa am grija sa nu fac asta
-        if (vecin != parent[node]) {
-          // am gasit o muchie de intoarcere
-          low_link[node] = std::min(low_link[node], idx[vecin]);
-        }
-      }
-    }
-
-    // daca nodul curent este radacina si are mai mult de 2 copii
-    if (parent[node] == 0 && children >= 2) {
-      cut_vertex.push_back((node));
-    }
   }
 
   void print_output() {
@@ -211,8 +233,8 @@ int main() {
   cin.rdbuf(fin.rdbuf()); // save and redirect
 
   // las liniile urmatoare daca afisez in fisier
-  ofstream fout("biconex.out");
-  cout.rdbuf(fout.rdbuf()); // save and redirect
+  // ofstream fout("biconex.out");
+  // cout.rdbuf(fout.rdbuf()); // save and redirect
 
   // aici este rezolvarea propriu-zisa
   Task *task = new Task();
