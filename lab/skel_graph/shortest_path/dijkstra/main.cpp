dbda3cf890c6617c76c9010752f10f63ed128216
Alexandru-Gabriel POPESCU (87516) <alexandru.popescu97@stud.acs.upb.ro>
2018-04-20 17:25:55 -0400

    dijkstra refactor

--- lab/skel_graph/shortest_path/dijkstra/main.cpp
+++ lab/skel_graph/shortest_path/dijkstra/main.cpp
@@ -1,157 +1,179 @@
-// Copyright 2018 Darius Neatu <neatudarius@gmail.com>
+// Copyright 2018 Popescu Alexandru Gabriel <alexandru.popescu0697@gmail.com>
+// Copyright 2017 Darius Neatu <neatudarius@gmail.com>
 
+// Dijkstra
+// O(m log n)
+// Deoarece folosesc heapuri binare (priority_queue),
+// complexitatea este cea de mai sus
 
-/*
-    skel_graph - Dijkstra - O(m log n)
-    (Sau cu notatia din lab O(E log V))
-    http://www.infoarena.ro/problema/dijkstra
+// http://www.infoarena.ro/problema/dijkstra
 
-    Deoarece folosesc heapuri binare (priority_queue),
-    complexitatea este cea de mai sus
-*/
+#include <bits/stdc++.h>
 
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <cstdio>
-#include <cstring>
-#include <fstream>
-#include <iostream>
-#include <queue> // std::priority_queue
-#include <vector>
-#define NMAX 50009   // ATENTIE la cat e in problema curenta !!!
-#define MMAX 250009  // ATENTIE la cat e in problema curenta !!!
+#define NMAX 50010   // ATENTIE la cat e in problema curenta !!!
 #define oo (1 << 30) // ATENTIE la cat e in problema curenta !!!
+#define NO_PARENT (-1)
+
 using namespace std;
 
-// tipul edge
-// muchia de cost "cost", de la node la neighbour,
-// va fi un element edge(neighbour, cost) in G[node]
-typedef pair<int, int> edge;
-#define neighbour first
-#define cost second
-
-// aceleasi semnificatii ca la BFS
-int n, m;
-int d[NMAX], p[NMAX];
-vector<edge> G[NMAX];
-
-// declararea unui priority queue in care pun numere intregi (noduri)
-// si cu un criteriu custom de prioritate
-
-// varianta 1
-// http://www.cplusplus.com/reference/queue/priority_queue/priority_queue/
-struct cmp {
-  // returneaza true daca x ar trebui "sa fie sub" y in arbore
-  bool operator()(const int &x, const int &y) const { return d[x] > d[y]; }
-};
-priority_queue<int, vector<int>, cmp> pq;
-
-// varianta 2 - C++11 :D
-// http://en.cppreference.com/w/cpp/language/decltype
-// bool cmp(const int &x, const int &y) {
-//     // returneaza true daca x ar trebui "sa fie sub" y in arbore
-//     return d[x] > d[y];
-// }
-// priority_queue<int, vector<int>, decltype(&cmp)> pq(cmp);
-
-void read_input() {
-  cin >> n >> m;
-
-  for (int i = 1; i <= m; ++i) {
-    int x, y, c; // x -> y, de cost c
-    cin >> x >> y >> c;
-
-    G[x].push_back(edge(y, c));
-    // G[y].push_back(edge(x, c)); // sterg daca e orientat !!!
-  }
-}
-
-void Dijkstra(int source) {
-  // ETAPA 1
-  for (int i = 1; i <= n; ++i) {
-    d[i] = oo;
-    p[i] = oo;
+class Task {
+public:
+  void solve() {
+    read_input();
+    get_result();
+    print_output();
   }
 
-  // ETAPA 2
-  pq.push(source);
-  d[source] = 0;
-  p[source] = 0;
+private:
+  // n = numar de noduri, m = numar de muchii
+  int n, m;
 
-  // ETAPA 3
-  while (!pq.empty()) {
-    int node = pq.top();
-    pq.pop();
+  int source;
 
-    for (auto it = G[node].begin(); it != G[node].end(); ++it) {
-      int neighbour = it->neighbour;
-      int cost_edge = it->cost;
+  // adj[i] contine perechi de tipul : vecin , costul muchiei i -> vecin
+  vector<pair<int, int>> adj[NMAX];
 
-      if (d[node] + cost_edge < d[neighbour]) {
-        d[neighbour] = d[node] + cost_edge;
-        p[neighbour] = node;
+  // say something
+  priority_queue<pair<int, int>, vector<pair<int, int>>,
+                 std::greater<pair<int, int>>>
+      pq;
 
-        pq.push(neighbour);
+  // d[i] = distanta minima de la de source la nodul i
+  vector<int> d;
+
+  // p[i] = parintele lui i de pe drumul minim de la source la nodul i
+  vector<int> p;
+
+  void read_input() {
+    cin >> n >> m;
+
+    d.resize(n + 1);
+
+    p.resize(n + 1);
+
+    // pe infoarena sursa este 1
+    // modificati linia urmatoare pentru a seta alta sursa
+    source = 1;
+
+    for (int i = 1; i <= m; ++i) {
+      // citim muchii x -> y, de cost c
+      int x, y, c;
+      cin >> x >> y >> c;
+
+      // adaugam in lista de adiacenta a nodului x
+      // nodul y cu costul muchiei x -> y = c
+      adj[x].push_back({y, c});
+    }
+  }
+
+  void get_result() { Dijkstra(source); }
+
+  void Dijkstra(int source) {
+    // ETAPA 1
+    for (int i = 1; i <= n; ++i) {
+      // presupun ca nu am drum
+      d[i] = oo;
+
+      // deci nici parinte (i inaccesibil)
+      p[i] = NO_PARENT;
+    }
+
+    // ETAPA 2
+
+    // parintele lui source este 0 - conventie
+    p[source] = 0;
+
+    // distanta de la source la source e 0
+    d[source] = 0;
+
+    pq.push({d[source], source});
+
+    // ETAPA 3
+    while (!pq.empty()) {
+      // extragem nodul care are costul estimat minim fata de sursa
+      auto entry = pq.top();
+      pq.pop();
+      int cost = entry.first;
+      int node = entry.second;
+
+      // daca intrarea curenta nu este up-to-date
+      if (cost > d[node]) {
+        continue;
+      }
+
+      // pentru fiecare vecin, incercam sa relaxam costul de la sursa
+      // la el trecand prin nodul node
+      for (auto &edge : adj[node]) {
+        int neighbour = edge.first;
+        int cost_edge = edge.second;
+
+        // daca obtinem un costmai mic trecand prin node
+        if (d[node] + cost_edge < d[neighbour]) {
+
+          // salvam noua distanta
+          d[neighbour] = d[node] + cost_edge;
+
+          // noul parinte devine node
+          p[neighbour] = node;
+
+          // actualizez costul drumului de la sursa la vecin
+          pq.push({d[neighbour], neighbour});
+        }
+      }
+    }
+
+    // ETAPA 4 - daca e cazul
+    // infoarena vrea sa nu afisez oo, ci 0
+    for (int i = 1; i <= n; ++i) {
+      if (d[i] == oo) {
+        d[i] = 0;
       }
     }
   }
 
-  // ETAPA 4 - daca e cazul
-  // infoarena vrea sa nu afisez oo, ci 0
-  for (int i = 1; i <= n; ++i) {
-    if (d[i] == oo) {
-      d[i] = 0;
+  // rebuild source -> ... -> node (if exists)
+  vector<int> rebuild_path(int node, vector<int> &p) {
+    // nu exista drum de la source la node
+    if (p[node] == NO_PARENT) {
+      return {};
     }
-  }
-}
 
-// in solve scriu tot ce tine de rezolvare - e ca un main
-void solve() {
-  // pe infoarena sursa este 1
-  Dijkstra(1);
-}
+    // path = {source, ..., node}
+    vector<int> path;
 
-// rebuild source -> ... -> node (if exists)
-void rebuild_path_recursive(int node, int source) {
-  if (d[node] == oo) {
-    cout << "Drum inexistent!";
-    return;
-  }
+    // rebuild node -> ... -> source (if exists)
+    for (; node != 0; node = p[node]) {
+      path.push_back(node);
+    }
+    // cand node == source, p[node] == 0
+    // (asta ma asigura ca forul se opreste la iteratia urmatoare)
 
-  if (node == source) { // sau p[node] = 0; // conditie echivalenta
-    cout << node << " ";
-    return;
+    // s-a obtinut drumul node -> ... -> source
+    // inversam solutia
+    reverse(path.begin(), path.end());
+
+    return path;
   }
 
-  // rebuild first source -> ...-> p[node]
-  rebuild_path_recursive(p[node], source);
-
-  // then p[node] -> node
-  cout << node << " ";
-}
-
-// afisez vectori si tot ce mai trebuie
-void print_output() {
-  // cout << "d: ";
-  for (int i = 2; i <= n; ++i) {
-    cout << d[i] << " ";
+  void print_output() {
+    for (int i = 1; i <= n; ++i) {
+      if (i == source) {
+        continue;
+      }
+      cout << "Distanta minima de la nodul " << source << " la nodul " << i
+           << " este ";
+      cout << d[i] << " ";
+      cout << "pe drumul:\n";
+      auto path = rebuild_path(i, p);
+      for (auto &node : path) {
+        cout << node << ' ';
+      }
+      cout << '\n';
+    }
+    cout << "\n";
   }
-  cout << "\n";
+};
 
-  // Exemplu: reconstituie drumul de la i la n
-
-  // cu recursivitate - de obicei pe asta o folosesc
-  // pentru ca daca nu vreau sa stochez drumul, o pot face
-  // cout << "rebuild_path_recursive\n";
-  // for (int i = 1; i <= n; ++i) {
-  //     cout << s << "->" << i << " : ";
-  //     rebuild_path_recursive(i, s); // rebuild s-> ... -> i
-  //     cout << "\n";
-  // }
-}
-
-// puteti pastra main-ul mereu cum e acum
 int main() {
   // din cauza ca fac redirectari, salvez starea lui cin si cout
   auto cin_buff = cin.rdbuf();
@@ -163,19 +185,19 @@ int main() {
 
   // las liniile urmatoare daca afisez in fisier
   // ofstream fout("dijkstra.out");
-  // cout.rdbuf(fout.rdbuf()); //save and redirect
+  // cout.rdbuf(fout.rdbuf()); // save and redirect
 
   // aici este rezolvarea propriu-zisa
-  read_input();
-  solve();
-  print_output();
+  Task *task = new Task();
+  task->solve();
+  delete task;
 
   // restore pentru cin si cout
   cin.rdbuf(cin_buff);
   cout.rdbuf(cout_buff);
 
   // obs. nu e nevoie sa inchid fisierele
-  // cand se apeleaz destructorii pentru fin si fout se vor inchide
+  // cand se apeleaza destructorii pentru fin si fout se vor inchide
 
   return 0;
 }
