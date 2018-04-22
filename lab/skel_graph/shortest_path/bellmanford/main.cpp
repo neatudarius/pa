dddd008acc45a36dadf62305a8add44ae682d96a
popescuAlexandru <38362302+popescuAlexandru@users.noreply.github.com>
2018-04-22 14:51:46 -0400

    bellman ford refactor (#15)
    bellman ford refactor tested on infoarena: https://www.infoarena.ro/job_detail/2197785

--- lab/skel_graph/shortest_path/bellmanford/main.cpp
+++ lab/skel_graph/shortest_path/bellmanford/main.cpp
@@ -1,150 +1,208 @@
-// Copyright 2018 Darius Neatu <neatudarius@gmail.com>
+// Copyright 2018 Popescu Alexandru Gabriel <alexandru.popescu0697@gmail.com>
+// Copyright 2017 Darius Neatu <neatudarius@gmail.com>
 
-/*
-    skel_graph - BellmanFord - O(mn)
-    (Sau cu notatia din lab O(EV))
-    http://www.infoarena.ro/problema/bellmanford
+// BellmanFord
+// O(m * n)
 
-    Varianta infoarena - simple as bfs :*
-    In practica facem altfel decat in laborator.
-    Imi place ca si coding style varianta asta.
+// http://www.infoarena.ro/problema/bellmanford
 
-    In plus, ma opresc daca un nod a fost folosit de n ori,
-    pentru ca atunci am gasit un ciclu de cost negativ.
-    => ca in practica fac < m * (n - 1) relaxari
-*/
+// In plus, ma opresc daca un nod a fost folosit de n ori,
+// pentru ca atunci am gasit un ciclu de cost negativ.
 
-#include <algorithm>
-#include <bitset>
-#include <cassert>
-#include <cstdio>
-#include <cstring>
-#include <fstream>
-#include <iostream>
-#include <queue>
-#include <vector>
-#define NMAX 50009   // ATENTIE la cat e in problema curenta !!!
-#define MMAX 250009  // ATENTIE la cat e in problema curenta !!!
+#include <bits/stdc++.h>
+
+#define NMAX 50010   // ATENTIE la cat e in problema curenta !!!
 #define oo (1 << 30) // ATENTIE la cat e in problema curenta !!!
+#define NO_PARENT (-1)
+
 using namespace std;
 
-// tipul edge
-// muchia de cost "cost", de la node la neighbour,
-// va fi un element edge(neighbour, cost) in G[node]
-typedef pair<int, int> edge;
-#define neighbour first
-#define cost second
-
-// aceleasi semnificatii ca la BFS
-int n, m;
-int d[NMAX], p[NMAX], used[NMAX];
-vector<edge> G[NMAX];
-
-// coada
-queue<int> Q;
-
-void read_input() {
-  cin >> n >> m;
-
-  for (int i = 1; i <= m; ++i) {
-    int x, y, c; // x -> y, de cost c
-    cin >> x >> y >> c;
-
-    G[x].push_back(edge(y, c));
-    // G[y].push_back(edge(x, c)); // sterg daca e orientat !!!
-  }
-}
-
-// return true = ciclu de cost negativ
-// return false = nu are ciclu de cost negativ
-bool BellmanFord(int source) {
-  // ETAPA 1
-  for (int i = 1; i <= n; ++i) {
-    d[i] = oo;
-    p[i] = oo;
+class Task {
+public:
+  void solve() {
+    read_input();
+    get_result();
   }
 
-  // ETAPA 2
-  Q.push(source);
-  d[source] = 0;
-  p[source] = 0;
+private:
+  // n = numar de noduri, m = numar de muchii
+  int n, m;
 
-  // ETAPA 3
-  while (!Q.empty()) {
-    int node = Q.front();
-    Q.pop();
+  // source = sursa din BellmanFord
+  int source;
 
-    ++used[node];
-    if (used[node] == n) {
-      return true; // am gasit ciclu de cost negativ
+  // adj[i] contine perechi de tipul : vecin , costul muchiei i -> vecin
+  vector<pair<int, int>> adj[NMAX];
+
+  queue<int> q;
+
+  // d[i] = distanta minima de la de source la nodul i
+  vector<int> d;
+
+  // p[i] = parintele lui i de pe drumul minim de la source la nodul i
+  vector<int> p;
+
+  void read_input() {
+    cin >> n >> m;
+
+    d.resize(n + 1);
+
+    p.resize(n + 1);
+
+    // pe infoarena sursa este 1
+    // modificati linia urmatoare pentru a seta alta sursa
+    source = 1;
+
+    for (int i = 1; i <= m; ++i) {
+      // citim muchii x -> y, de cost c
+      int x, y, c;
+      cin >> x >> y >> c;
+
+      // adaugam in lista de adiacenta a nodului x
+      // nodul y cu costul muchiei x -> y = c
+      adj[x].push_back({y, c});
+    }
+  }
+
+  void get_result() {
+    if (BellmanFord(source, d, p)) {
+      cout << "Ciclu negativ!";
+    } else {
+      print_output();
+    }
+  }
+
+  bool BellmanFord(int source, vector<int> &d, vector<int> &p) {
+    // ETAPA 1
+
+    // used[i] = de cate ori a fost folosit nodul i
+    vector<int> used(n + 1, 0);
+
+    for (int i = 1; i <= n; ++i) {
+      // presupun ca nu am drum
+      d[i] = oo;
+
+      // deci nici parinte (i inaccesibil)
+      p[i] = NO_PARENT;
     }
 
-    for (auto it = G[node].begin(); it != G[node].end(); ++it) {
-      int neighbour = it->neighbour;
-      int cost_edge = it->cost;
+    // ETAPA 2
 
-      if (d[node] + cost_edge < d[neighbour]) {
-        d[neighbour] = d[node] + cost_edge;
-        p[neighbour] = node;
+    // parintele lui source este 0 - conventie
+    p[source] = 0;
 
-        Q.push(neighbour);
+    // distanta de la source la source e 0
+    d[source] = 0;
+
+    q.push(source);
+
+    // ETAPA 3
+    while (!q.empty()) {
+      int node = q.front();
+      q.pop();
+
+      // cresc numarul de utilizari al nodului node
+      ++used[node];
+      if (used[node] == n) {
+        return true; // am gasit ciclu de cost negativ
+      }
+
+      // pentru fiecare vecin, incercam sa relaxam costul de la sursa
+      // la el trecand prin nodul node
+      for (auto &edge : adj[node]) {
+        int neighbour = edge.first;
+        int cost_edge = edge.second;
+
+        // daca obtinem un cost mai mic trecand prin node
+        if (d[node] + cost_edge < d[neighbour]) {
+
+          // salvam noua distanta
+          d[neighbour] = d[node] + cost_edge;
+
+          // noul parinte devine node
+          p[neighbour] = node;
+
+          // actualizez costul drumului de la sursa la vecin
+          q.push(neighbour);
+        }
       }
     }
+
+    // ETAPA 4 - daca este cazul
+    return false;
   }
 
-  // ETAPA 4 - daca e cazul
-  return false;
-}
+  // rebuild source -> ... -> node (if exists)
+  vector<int> rebuild_path(int node, vector<int> &p) {
+    // nu exista drum de la source la node
+    if (p[node] == NO_PARENT) {
+      return {};
+    }
 
-// in solve scriu tot ce tine de rezolvare - e ca un main
-void solve() {
-  // pe infoarena sursa este 1
-  if (BellmanFord(1)) {
-    cout << "Ciclu negativ!\n";
-  } else {
-    for (int i = 2; i <= n; ++i) {
+    // path = {source, ..., node}
+    vector<int> path;
+
+    // rebuild node -> ... -> source (if exists)
+    for (; node != 0; node = p[node]) {
+      path.push_back(node);
+    }
+    // cand node == source, p[node] == 0
+    // (asta ma asigura ca forul se opreste la iteratia urmatoare)
+
+    // s-a obtinut drumul node -> ... -> source
+    // inversam solutia
+    reverse(path.begin(), path.end());
+
+    return path;
+  }
+
+  void print_output() {
+    for (int i = 1; i <= n; ++i) {
+      if (i == source) {
+        continue;
+      }
+      if (d[i] == 0) {
+        cout << "Nu exista drum de la " << source << " la nodul " << i << '\n';
+        continue;
+      }
+      cout << "Distanta minima de la nodul " << source << " la nodul " << i
+           << " este ";
       cout << d[i] << " ";
+      cout << "pe drumul:\n";
+      auto path = rebuild_path(i, p);
+      for (auto &node : path) {
+        cout << node << ' ';
+      }
+      cout << '\n';
     }
     cout << "\n";
   }
-}
+};
 
-// rebuild source -> ... -> node (if exists)
-void rebuild_path_recursive(int node, int source) {
-  if (d[node] == oo) {
-    cout << "Drum inexistent!";
-    return;
-  }
-
-  if (node == source) { // sau p[node] = 0; // conditie echivalenta
-    cout << node << " ";
-    return;
-  }
-
-  // rebuild first source -> ...-> p[node]
-  rebuild_path_recursive(p[node], source);
-
-  // then p[node] -> node
-  cout << node << " ";
-}
-
-// puteti pastra main-ul mereu cum e acum
 int main() {
-  // las linia urmatoare daca citesc din fisier
-  assert(freopen("bellmanford.in", "r", stdin) != NULL);
+  // din cauza ca fac redirectari, salvez starea lui cin si cout
+  auto cin_buff = cin.rdbuf();
+  auto cout_buff = cout.rdbuf();
 
-  // las linia urmatoare daca afisez in fisier
-  assert(freopen("bellmanford.out", "w", stdout) != NULL);
+  // las liniile urmatoare daca citesc din fisier
+  ifstream fin("bellmanford.in");
+  cin.rdbuf(fin.rdbuf()); // save and redirect
 
-  read_input();
-  solve();
-  // print_output();
+  // las liniile urmatoare daca afisez in fisier
+  // ofstream fout("bellmanford.out");
+  // cout.rdbuf(fout.rdbuf()); // save and redirect
+
+  // aici este rezolvarea propriu-zisa
+  Task *task = new Task();
+  task->solve();
+  delete task;
+
+  // restore pentru cin si cout
+  cin.rdbuf(cin_buff);
+  cout.rdbuf(cout_buff);
+
+  // obs. nu e nevoie sa inchid fisierele
+  // cand se apeleaza destructorii pentru fin si fout se vor inchide
 
   return 0;
 }
-
-// Obs: pe infoarena ia 85p din cauza citirii (ignorati faptul ca testele sunt
-// grupate, nu ne intereseaza asa ceva la PA :D)
-// http://www.infoarena.ro/job_detail/1970189
-// oricum mai rapid decat pseudocodul din laborator
-// desigur.. il puteti adapta sa se opreasca mai devreme, daca vreti
