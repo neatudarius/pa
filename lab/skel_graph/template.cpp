49092d51b0337a72db699f3873c314407a7f3122
Darius Neatu <neatudarius@gmail.com>
2020-05-31 20:31:33 +0300

    use std everywhere

--- lab/skel_graph/template.cpp
+++ lab/skel_graph/template.cpp
@@ -26,7 +26,7 @@ private:
     int n, m;
 
     // adj[node] lista de adiacenta a nodului node
-    vector<int> adj[NMAX];
+    std::vector<int> adj[NMAX];
 
     void read_input() { }
 
@@ -36,26 +36,26 @@ private:
 };
 
 int main() {
-    // din cauza ca fac redirectari, salvez starea lui cin si cout
-    auto cin_buff  = cin.rdbuf();
-    auto cout_buff = cout.rdbuf();
+    // din cauza ca fac redirectari, salvez starea lui std::cin si std::cout
+    auto cin_buff  = std::cin.rdbuf();
+    auto cout_buff = std::cout.rdbuf();
 
     // las liniile urmatoare daca citesc din fisier
     ifstream fin("in");
-    cin.rdbuf(fin.rdbuf()); // save and redirect
+    std::cin.rdbuf(fin.rdbuf()); // save and redirect
 
     // las liniile urmatoare daca afisez in fisier
     // ofstream fout("out");
-    // cout.rdbuf(fout.rdbuf()); //save and redirect
+    // std::cout.rdbuf(fout.rdbuf()); //save and redirect
 
     // aici este rezolvarea propriu-zisa
     Task* task = new Task();
     task->solve();
     delete task;
 
-    // restore pentru cin si cout
-    cin.rdbuf(cin_buff);
-    cout.rdbuf(cout_buff);
+    // restore pentru std::cin si std::cout
+    std::cin.rdbuf(cin_buff);
+    std::cout.rdbuf(cout_buff);
 
     // obs. nu e nevoie sa inchid fisierele
     // cand se apeleaza destructorii pentru fin si fout se vor inchide
